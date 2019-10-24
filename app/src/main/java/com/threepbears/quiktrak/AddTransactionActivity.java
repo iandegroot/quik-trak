89f4ddb31782401c6465331f3b6888de2ad5e53f
Ian DeGroot <ianpdegroot@gmail.com>
2019-10-23 22:36:07 -0500

    Adding calendar popup when clicking on date text field.

--- app/src/main/java/com/threepbears/quiktrak/AddTransactionActivity.java
+++ app/src/main/java/com/threepbears/quiktrak/AddTransactionActivity.java
@@ -1,10 +1,8 @@
 package com.threepbears.quiktrak;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -12,8 +10,11 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+
+import java.util.Calendar;
 
 public class AddTransactionActivity extends AppCompatActivity {
 
@@ -32,6 +33,26 @@ public class AddTransactionActivity extends AppCompatActivity {
         final Spinner categorySpinner = findViewById(R.id.categorySpinner);
         final EditText noteEditText = findViewById(R.id.noteEditText);
         final Button addTransButton = findViewById(R.id.addTransactionButton);
+
+        dateEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar calendar = Calendar.getInstance();
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                int month = calendar.get(Calendar.MONTH);
+                int year = calendar.get(Calendar.YEAR);
+
+                DatePickerDialog datePicker = new DatePickerDialog(AddTransactionActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        final String date = (month+1) + "/" + day + "/" + year;
+                        dateEditText.setText(date);
+                    }
+                }, year, month, day);
+
+                datePicker.show();
+            }
+        });
 
         addTransButton.setOnClickListener(new View.OnClickListener() {
             @Override
