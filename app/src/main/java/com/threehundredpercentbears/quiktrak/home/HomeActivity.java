0ce6fab525b1563f918ed134b11fe34692e3643e
Ian DeGroot <ianpdegroot@gmail.com>
2019-12-22 22:09:42 -0600

    Adding menu with Add Transaction option to HomeActivity.

--- app/src/main/java/com/threehundredpercentbears/quiktrak/home/HomeActivity.java
+++ app/src/main/java/com/threehundredpercentbears/quiktrak/home/HomeActivity.java
@@ -10,6 +10,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.google.android.material.tabs.TabLayout;
@@ -54,5 +56,27 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(new Intent(HomeActivity.this, AddTransactionActivity.class));
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        
+        if (id == R.id.add_transactions_page) {
+            startActivity(new Intent(HomeActivity.this, AddTransactionActivity.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
